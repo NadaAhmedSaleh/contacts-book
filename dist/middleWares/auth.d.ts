693b89ef6ee502a64823547230859869d0dac84f
Nada Saleh <nada.saleh@mazad.at>
2023-08-01 03:03:51 +0300

    delete dist

--- dist/middleWares/auth.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-interface AuthenticatedRequest extends Request {
-    user: {
-        id: string;
-    };
-}
-declare const auth: (req: AuthenticatedRequest, res: Response, next: NextFunction) => Promise<void>;
-export default auth;
-export { AuthenticatedRequest };
